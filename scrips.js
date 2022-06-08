65123fadf1657619b0e524bb9ac04f82f086a85a
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-06-08 21:47:07 +0200

    close icon

--- scrips.js
+++ scrips.js
@@ -1,9 +1,15 @@
 const burgur = document.getElementById('burger');
+const close = document.getElementById('close');
 const navbar = document.getElementById('nav-bar');
 
 
 if(burgur){
     burgur.addEventListener('click',()=>{
         navbar.classList.add('active');
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        navbar.classList.remove('active');
     })
 }
