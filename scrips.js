94b90f4476350b7e4878d47c15febd8447eea2a3
Marinkie <69327534+Marinkiet@users.noreply.github.com>
2022-06-10 01:38:34 +0200

    single product page and small device styling

--- scrips.js
+++ scrips.js
@@ -13,3 +13,20 @@ if(close){
         navbar.classList.remove('active');
     })
 }
+
+//single product
+let mainImg = document.getElementById("main-prod-img");
+let smallImg = document.getElementsByClassName("small-img");
+
+smallImg[0].onclick = () =>{
+    mainImg.src = smallImg[0].src;
+}
+smallImg[1].onclick = () =>{
+    mainImg.src = smallImg[1].src;
+}
+smallImg[2].onclick = () =>{
+    mainImg.src = smallImg[2].src;
+}
+smallImg[3].onclick = () =>{
+    mainImg.src = smallImg[3].src;
+}
